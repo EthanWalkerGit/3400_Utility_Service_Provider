6bf9eb37b169d2fec934cfbde623b8eee670df10
ah-shi <123603788+adriang76@users.noreply.github.com>
2025-03-20 12:23:59 -0400

    Add files via upload

---
+++ src/UtilityService.h
@@ -0,0 +1,27 @@
+#ifndef UTILITYSERVICE_H
+#define UTILITYSERVICE_H
+#include <iostream>
+using namespace std;
+
+class UtilityService{
+private:
+	int service_id;
+	string service_name;
+	double rate_per_unit;
+	double fixed_charge;
+	int providerID;
+public:
+	UtilityService(int sid, string sn,double rpu, double fc, int pid);
+
+	double getRate() const;
+
+	double calculateBill(double units_used) const;
+
+	string getName() const;
+
+	int getSID() const;
+
+	double getFC() const;
+
+};
+#endif
