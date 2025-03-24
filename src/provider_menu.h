096b2922eeebc75007c0d7a3fb7cd99c792ce59b
ah-shi <123603788+adriang76@users.noreply.github.com>
2025-03-24 17:24:01 -0400

    Add files via upload

---
+++ src/provider_menu.h
@@ -0,0 +1,21 @@
+#ifndef PROVIDER_MENU
+#define PROVIDER_MENU
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include "UtilityService.h"
+#include "provider.h"
+#include "databaseManager.h"
+
+
+using namespace std;
+
+void provider_menu(vector<provider>& providers, vector<UtilityService>& services , DatabaseManager& dbManager);
+    
+
+
+
+
+#endif
