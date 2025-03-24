18ba451f56e5331bb497d7da46464290befd0d2f
ah-shi <123603788+adriang76@users.noreply.github.com>
2025-03-24 17:21:45 -0400

    Update UtilityService.cpp

--- src/UtilityService.cpp
+++ src/UtilityService.cpp
@@ -1,31 +1,32 @@
 #include "UtilityService.h"
 using namespace std;
 
-UtilityService::UtilityService(int sid, string sn, double rpu, double fc, int pid)
-    : service_id(sid), service_name(sn), rate_per_unit(rpu), fixed_charge(fc), providerID(pid) {}
 
-// ************************************************************
-//
-//  Function: getRate
-//
-//  Description: Returns the rate per unit for the service.
-//
-// ************************************************************
 
-double UtilityService::getRate() const { return rate_per_unit; }
+UtilityService::UtilityService(int sid, string sn, double rpu, double fc , int pid)
+	: service_id(sid), service_name(sn), rate_per_unit(rpu), fixed_charge(fc), providerID(pid){}
 
-// ************************************************************
-//
-//  Function: calculateBill
-//
-//  Description: Calculates the bill for a given usage.
-//
-// ************************************************************
+UtilityService::~UtilityService(){}
 
-double UtilityService::calculateBill(double units_used) const { return (rate_per_unit + units_used) + fixed_charge; }
+double UtilityService::getRate()const { return rate_per_unit;}
 
-string UtilityService::getName() const { return service_name; }
 
-int UtilityService::getSID() const { return service_id; }
 
-double UtilityService::getFC() const { return fixed_charge; }
+double UtilityService::calculateBill(double units_used) const{ return (rate_per_unit + units_used) + fixed_charge;}
+
+
+string UtilityService::getName() const {return service_name;}
+
+
+int UtilityService::getSID() const {return service_id;}
+
+double UtilityService::getFC()const { return fixed_charge;}
+
+int UtilityService::getPID() const {return providerID;}
+
+void UtilityService::edit (double new_rate, double new_fc) {
+	rate_per_unit = new_rate;
+	fixed_charge = new_fc;
+
+}
+
