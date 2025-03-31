43fa7e18622bbc5ab6a4f25e99d676bbdc6c63fc
ah-shi <123603788+adriang76@users.noreply.github.com>
2025-03-31 18:20:03 -0400

    Update provider.cpp

--- src/provider.cpp
+++ src/provider.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 
-provider::provider(int id, string name) : provider_id(id) , provider_name(name){}
+provider::provider(int id, string name, double s) : provider_id(id) , provider_name(name), sales(s){}
 
 string provider::getName() const {return provider_name;}
 
 int provider::get_pid() const { return provider_id;}
+
